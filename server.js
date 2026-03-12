0f2577eb63791f59228274efe8d6c05d03f8b3cc
Sheikh Antor <antoralways2025@gmail.comm>
2026-03-13 03:18:11 +0600

    Update server.js with signaling fixes / socket handling

--- server.js
+++ server.js
@@ -2,46 +2,37 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const Port = process.env.PORT;
+
 const app = express();
 app.use(cors());
 
-// HTTP server create
 const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
-// Socket.io server create
-const io = new Server(server, {
-  cors: {
-    origin: "*"  // development এ সব origin allow, production এ specific ঠিক করা যাবে
-  }
-});
-
-// Socket connection handle
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  // Call event
+  // Caller sends offer
   socket.on("call-user", (data) => {
+    // data = { to: id, from: id, offer: {type, sdp} }
     io.to(data.to).emit("receive-call", data);
   });
 
-  // Answer event
+  // Receiver sends answer
   socket.on("make-answer", (data) => {
     io.to(data.to).emit("answer-made", data);
   });
 
-  // ICE candidate event
+  // ICE candidates
   socket.on("ice-candidate", (data) => {
     io.to(data.to).emit("ice-candidate", data.candidate);
   });
 
-  // Disconnect event
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
   });
 });
 
-// Server listen
-server.listen(Port, () => {
+server.listen(5000, () => {
   console.log("Server running on port 5000");
 });
