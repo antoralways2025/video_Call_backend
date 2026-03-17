302aac07e919b23d82960acf4d88559d9641792a
Sheikh Antor <antoralways2025@gmail.comm>
2026-03-18 03:26:49 +0600

    fixing same network issus

--- server.js
+++ server.js
@@ -7,29 +7,49 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// store users
+const users = {};
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  // Caller sends offer
+  // save user
+  users[socket.id] = socket.id;
+
+  // CALL USER
   socket.on("call-user", (data) => {
-    // data = { to: id, from: id, offer: {type, sdp} }
-    io.to(data.to).emit("receive-call", data);
+    io.to(data.to).emit("receive-call", {
+      from: socket.id,
+      offer: data.offer,
+    });
   });
 
-  // Receiver sends answer
+  // ANSWER CALL
   socket.on("make-answer", (data) => {
-    io.to(data.to).emit("answer-made", data);
+    io.to(data.to).emit("answer-made", {
+      from: socket.id,
+      answer: data.answer,
+    });
   });
 
-  // ICE candidates
+  // ICE CANDIDATE
   socket.on("ice-candidate", (data) => {
-    io.to(data.to).emit("ice-candidate", data.candidate);
+    io.to(data.to).emit("ice-candidate", {
+      from: socket.id,
+      candidate: data.candidate,
+    });
   });
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
+    delete users[socket.id];
   });
 });
 
